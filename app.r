57b753b0f28edd719079718acf278d1955246c99
Howard <howard.swerdfeger@canada.ca>
2020-03-21 12:53:40 -0400

    Update app.r

--- app.r
+++ app.r
@@ -2,13 +2,14 @@ library(shiny)
 library(shinyjs)
 library(leaflet)
 source("leaftlet_nrc.r")
+
 r_colors <- rgb(t(col2rgb(colors()) / 255))
 names(r_colors) <- colors()
 
 ui <- fluidPage(
     #get_leaflet_map(),
-    leafletOutput("mymap"),
-    p()#,
+    leafletOutput("mymap")#,
+    #p()#,
     #actionButton("recalc", "New points")
 )
 
@@ -35,10 +36,10 @@ server <- function(input, output, session) {
         data_points
         #cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
     }, ignoreNULL = FALSE)
-    
+    #pal(data_circles$size)
     output$mymap <- renderLeaflet({
          get_leaflet_map() %>%
-             addCircles(data = data_circles, radius = ~ Size , fillOpacity = 0.8, popup = ~ Popup, color = "#0F3" , fillColor  = "#03F" ) #%>%
+             addCircles(data = data_circles, radius = ~ Size , fillOpacity = 0.8, popup = ~ Popup, color = "#0F3" , fillColor  = ~ pal(Size) ) #%>%
              #addMarkers(data = data_points, popup = ~City)
     })
 }
